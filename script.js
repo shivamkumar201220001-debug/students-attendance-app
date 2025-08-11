8eeec69093d0d148e873bdb092b4afc8c577be79
shivamkumar201220001-debug <shivamkumar201220001@gmail.com>
2025-08-11 18:59:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const SHEET_ID = "1qeHqI_WgkE7mmsWs1vwOQnKvtXojoH-TVXaQ0FcVMLI";
 const API_KEY = "AIzaSyBoQWKF1OjHI-rDK7BjFZHmhCyxvEx5XS8";
-const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbyvD7fW-ramfN07McE9w9tJ0dLHjbAPw9XZQgBizYicf5OT1bx5cRMDHXQVFVjDhS0I3g/exec?action=getData
-";
+const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbw1j3a-qWgleho9rMf4Su3ROm0zCtQuUftEHMG3fgauwmuHHqxan0A6OwCBCIkym0mHjA/exec?action=getData";
 
 const classSelect = document.getElementById("classSelect");
 const loadBtn = document.getElementById("loadBtn");
