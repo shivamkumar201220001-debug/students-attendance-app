8b47a2515001420a04efa2d9d33e55ff1787517c
shivamkumar201220001-debug <shivamkumar201220001@gmail.com>
2025-09-01 13:31:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /***** CONFIG *****/
-const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbxrnYGCz8HZmMYZTj_mCKcRjCkNQ3819jG7FFpnZJ92NH3onWwSxGQeBTLkC8bgHzXaJw/exec"; // e.g., https://script.google.com/macros/s/XXX/exec
+const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzyh7p2BodSAgHyR_pg8c0kIsskxCeYdcgYEzVhwzcW2j1EIlhAdo5ecxPql_zTmqrJEQ/exec"; // e.g., https://script.google.com/macros/s/XXX/exec
 
 // teacher list (keep in sync with backend)
 const TEACHERS = [
